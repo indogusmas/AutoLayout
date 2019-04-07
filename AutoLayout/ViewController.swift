563b55e35a0cc53f6951972f5075a4ad68addd08
indo <indogusmasarungsamudra@gmail.com>
2019-04-07 09:57:12 +0700

    initial commmit

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    //let'a avoid polluting viewDidload
+    //{} is referred to as closure, or anon, funtions
+    
+    
+    let mountainImg : UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "mountain") )
+        return imageView
+    }()
+    
+    func mountainImage() -> UIImageView {
+        let imageView = UIImageView(image: UIImage(named: "mountain") )
+        return imageView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //view.backgroundColor = UIColor.red
+        //  let imageView = UIImageView(image: UIImage(named: "mountain") )
+        view.addSubview(mountainImg)
+        setupImage()
+        
     }
-
-
+    
+    private func setupImage(){
+        // this code for setting position
+        //imageView.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
+        mountainImg.translatesAutoresizingMaskIntoConstraints = false
+        mountainImg.centerXAnchor.constraint(equalTo: view.centerXAnchor) .isActive = true
+        mountainImg.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        mountainImg.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        mountainImg.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
 }
 
